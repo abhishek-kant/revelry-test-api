8f36d87823c2ef83dfb3977e496556680c676290
Abhishek <abhi1206118@gmail.com>
2016-09-14 19:29:36 +0530

    show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.limit(15)
   end
+
+  def show
+    @article = Article.friendly.find(params[:id])
+  end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -6,8 +6,21 @@ RSpec.describe "Articles", :type => :request do
       FactoryGirl.create_list(:article, 10)
       get articles_path
       articles = JSON.parse(response.body)
+
       expect(response).to have_http_status(200)
       expect(articles.length).to eq(10)
+    end
+  end
+
+  describe "GET /article/:id" do
+    it "returns an by name" do
+      article = FactoryGirl.create(:article)
+
+      get article_path(article)
+      article_from_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(article_from_response).to eq(article)
     end
   end
 
